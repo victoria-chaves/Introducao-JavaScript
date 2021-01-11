fd281d10e8852c946fb530593a8463d08a74afd1
Victoria <victorianchaves@hotmail.com>
2021-01-11 18:47:04 -0300

    commit1: arquivos introdutorios

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+var nome = " Victoria";
+var idade = 23;
+var idade2 = 2;
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2); //soma os inteiros
+var frase = "Comida japonesa é top"
+console.log(frase);
+console.log(frase.replace("japonesa", "brasileira"));
+console.log(frase.toUpperCase());
+// pra comentar usa barra barra
+/* assim pode comentar varias linhas de uma vez*/
+
+var dicionario = {nome:"uva",cor:"roxa"};  
+var dicionario2 = {{nome:"uva",cor:"roxa"},{nome:"maca",cor:"vermelha"}}; //lista de dicionarios
